6ade615609dc5a5c00453b5e11a33bc654d76848
Harold.parra <Harold.parra@t-evolvers.com>
2021-12-08 21:49:59 -0500

    funcionalidad perfil

--- src/app/pages/perfil/perfil/resumen/resumen.component.ts
+++ src/app/pages/perfil/perfil/resumen/resumen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VehiclesService } from 'src/app/services/vehicles.service';
 
 @Component({
   selector: 'app-resumen',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resumen.component.css']
 })
 export class ResumenComponent implements OnInit {
+  activas:number;
+  pausadas:number;
+  finalizadas:number;
 
-  constructor() { }
+  constructor(private vehicleSv: VehiclesService) { 
+    this.getCount();
+  }
 
   ngOnInit(): void {
   }
 
+  getCount(){
+    this.vehicleSv.getVehiclesByIdState(localStorage.getItem('userid'), 'activa').subscribe(resp=>{
+      this.activas = resp.length;
+    })
+    this.vehicleSv.getVehiclesByIdState(localStorage.getItem('userid'), 'creado').subscribe(resp=>{
+      this.pausadas = resp.length;
+    })
+    this.vehicleSv.getVehiclesByIdState(localStorage.getItem('userid'), 'finalizada').subscribe(resp=>{
+      this.finalizadas = resp.length;
+    })
+  }
 }
